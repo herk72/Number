23d6c9ab6b32f93b9bc9995f3ccbe56740ca260d
herk72 <harkherk86@gmail.com>
2026-05-14 18:20:24 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,15 @@
-BOT_TOKEN = "8755998023:AAEDsAnAX-egzF-r973adB-_c2brcf-OUfA"
+# config.py
+BOT_TOKEN = "8990980316:AAGtdDq2USPMtXUOFLUeRvnTNy9pyS59WyE"
 API_ID = 37698652
 API_HASH = "58b8a290e85dd6e57127270d937a1832"
 REGISTRATION_LINK = "https://vimeo.com/1182266152?fl=pl&fe=cm"
 
+# --- إعدادات البريد الجديدة ---
+BASE_EMAIL = "plyywe23@gmail.com"  # البريد الأساسي
+EMAIL_USER = "plyywe23@gmail.com"  # بريدك لتسجيل الدخول
+EMAIL_PASS = "csto vyhz lcsa droq" # كلمة مرور التطبيقات (App Password)
+IMAP_SERVER = "imap.gmail.com"
 
-_A1 = 8151308428
+_A1 = 8357381411
 
-_A2 = 7551400544
-
-ADMIN_IDS = [_A1, _A2]
+ADMIN_IDS = [_A1]
